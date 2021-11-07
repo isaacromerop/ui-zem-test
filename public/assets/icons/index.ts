282668abc7b47917b969bf62e513d9d80831a656
iromero <iromero@lean-tech.io>
2021-11-07 17:12:23 -0500

    feat(assets/icons): create icon components

---
+++ public/assets/icons/index.ts
@@ -0,0 +1,8 @@
+export { Close } from "./close";
+export { Facebook } from "./facebook";
+export { Hamburguer } from "./hamburguer";
+export { Search } from "./search";
+export { ThumbDown } from "./thumb-down";
+export { ThumbUp } from "./thumb-up";
+export { Twitter } from "./twitter";
+export { Wikipedia } from "./wikipedia";
