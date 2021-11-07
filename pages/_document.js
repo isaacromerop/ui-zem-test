dc2b482067e7c0e3ba08b97eb63c46e2b2ed4772
iromero <iromero@lean-tech.io>
2021-11-07 17:10:34 -0500

    feat(tailwind.config.js): added colors to tailwind config

--- pages/_document.js
+++ pages/_document.js
@@ -12,7 +12,7 @@ class MyDocument extends Document {
             crossOrigin="true"
           />
           <link
-            href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700&display=swap"
             rel="stylesheet"
           />
         </Head>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 const { theme } = require("tailwindcss/defaultConfig");
+const colors = require("tailwindcss/colors");
 
 module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
@@ -8,6 +9,24 @@ module.exports = {
       fontFamily: {
         sans: ['"Lato"', ...theme.fontFamily.sans],
       },
+    },
+    colors: {
+      transparent: "transparent",
+      orange: "#FF4E00",
+      teal: "#3CBBB4",
+      gray: {
+        50: "#EBEBEB ",
+        100: "#F7F8FF",
+        200: "#D9DCEF",
+        300: "#8280A8",
+        400: "#100050",
+      },
+      green: { 100: "#44D7B6", 200: "#12CA68" },
+      red: { 100: "#FF647C", 200: "#E02020" },
+      yellow: { 100: "#FDC718", 200: "#F7B500", 300: "#F9AD1D" },
+      blue: { 100: "#14B8E7", 200: "#0091FF" },
+      white: colors.white,
+      black: colors.black,
     },
   },
   variants: {
