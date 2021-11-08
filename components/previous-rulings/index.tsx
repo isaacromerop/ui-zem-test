7e517155bab20e8c424b6a75b7f8f9140c6ff8a9
iromero <iromero@lean-tech.io>
2021-11-07 23:09:18 -0500

    fix(component/previous-rulings): component previous-rulings updated behaving different on screensize

--- components/previous-rulings/index.tsx
+++ components/previous-rulings/index.tsx
@@ -1,34 +1,77 @@
-import ScrollContainer from "react-indiana-drag-scroll";
-import { Card } from "../card";
 import { mockedData } from "../../mocks/data";
+import { MobilePreviousRulings } from "./mobile-previous-rulings";
+import { TabletDesktopPreviousRulings } from "./tablet-desktop-previous-rulings";
+import { Listbox, Transition } from "@headlessui/react";
+import { FaCaretDown, FaCheck } from "react-icons/fa";
+import { Fragment, useState } from "react";
+
+const views = [{ name: "List" }, { name: "Grid" }];
 
 const PreviousRulings = (): React.ReactElement => {
+  const [selectedValue, setSelectedValue] = useState(views[0]);
+
   return (
     <div>
-      <p className="text-2xl font-light px-3.5 lg:pl-32">Previous Rulings</p>
-      <ScrollContainer className="h-80 flex gap-x-4" vertical={false}>
-        <section className="px-4 mr-2 h-full flex w-full justify-between">
-          {mockedData.data.map(
-            ({ votes, category, name, description, picture, lastUpdated }) => (
-              <div
-                key={name}
-                className="cursor-move flex items-center justify-center w-72 h-72 flex-1 my-8 mx-2"
-              >
-                <div className="w-72 mr-4">
-                  <Card
-                    name={name}
-                    votes={votes}
-                    category={category}
-                    picture={picture}
-                    description={description}
-                    lastUpdated={lastUpdated}
-                  />
-                </div>
-              </div>
-            )
-          )}
-        </section>
-      </ScrollContainer>
+      <div className="flex justify-between px-3.5">
+        <p className="text-2xl font-light lg:px-32">Previous Rulings</p>
+        <div className="lg:px-32">
+          <Listbox value={selectedValue} onChange={setSelectedValue}>
+            <Listbox.Button className="relative w-32 border-2 border-black text-left px-5">
+              <span className="block truncate">{selectedValue.name}</span>
+              <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+                <FaCaretDown
+                  className="w-5 h-5 text-gray-400"
+                  aria-hidden="true"
+                />
+              </span>
+            </Listbox.Button>
+            <Transition
+              as={Fragment}
+              leave="transition ease-in duration-100"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Listbox.Options className="absolute z-10 w-32 border-2 border-black py-1 mt-1 overflow-auto text-base bg-white shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                {views.map((view) => (
+                  <Listbox.Option
+                    key={view.name}
+                    className={({ active }) =>
+                      `${active ? "text-black bg-gray-50" : "text-black"}
+                          cursor-default select-none relative py-2 pl-10 pr-4`
+                    }
+                    value={view}
+                  >
+                    {({ selected, active }) => (
+                      <>
+                        <span
+                          className={`${
+                            selected ? "font-medium" : "font-normal"
+                          } block truncate`}
+                        >
+                          {view.name}
+                        </span>
+                        {selected ? (
+                          <span
+                            className={`${active ? "text-black" : "text-black"}
+                                absolute inset-y-0 left-0 flex items-center pl-3`}
+                          >
+                            <FaCheck className="w-5 h-5" aria-hidden="true" />
+                          </span>
+                        ) : null}
+                      </>
+                    )}
+                  </Listbox.Option>
+                ))}
+              </Listbox.Options>
+            </Transition>
+          </Listbox>
+        </div>
+      </div>
+      <MobilePreviousRulings data={mockedData.data} />
+      <TabletDesktopPreviousRulings
+        data={mockedData.data}
+        view={selectedValue.name}
+      />
     </div>
   );
 };
