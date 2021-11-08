d9c880e0510d94fb12e4409bca665874facd0766
iromero <iromero@lean-tech.io>
2021-11-07 23:11:40 -0500

    feat(pages): component pages updated to load custom components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,20 @@
 import type { NextPage } from "next";
+import { Fragment } from "react";
+import { AddPerson } from "../components/add-person";
+import { BeCounted } from "../components/be-counted";
 import { Hero } from "../components/hero";
 import { Layout } from "../components/layout";
+import { PreviousRulings } from "../components/previous-rulings";
 
 const Home: NextPage = () => {
   return (
     <Layout>
-      <Hero />
+      <Fragment>
+        <Hero />
+        <BeCounted />
+        <PreviousRulings />
+        <AddPerson />
+      </Fragment>
     </Layout>
   );
 };
