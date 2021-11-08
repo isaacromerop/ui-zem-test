9044f7efa9f63ed3a1503439fb6c1739e201d41b
iromero <iromero@lean-tech.io>
2021-11-07 23:10:11 -0500

    feat(mobile-previous-rulings): component mobile-previous-rulings created

---
+++ components/previous-rulings/mobile-previous-rulings.tsx
@@ -0,0 +1,50 @@
+import ScrollContainer from "react-indiana-drag-scroll";
+import { Card } from "../card";
+
+type MobilePreviousRulingsProps = {
+  data: Array<{
+    name: string;
+    description: string;
+    category: string;
+    picture: string;
+    lastUpdated: string;
+    votes: {
+      positive: number;
+      negative: number;
+    };
+  }>;
+};
+
+const MobilePreviousRulings = ({
+  data,
+}: MobilePreviousRulingsProps): React.ReactElement => {
+  return (
+    <div className="md:hidden">
+      <ScrollContainer className="h-80 flex gap-x-4" vertical={false}>
+        <section className="px-4 mr-2 h-full flex w-full justify-between">
+          {data.map(
+            ({ votes, category, name, description, picture, lastUpdated }) => (
+              <div
+                key={name}
+                className="cursor-move flex items-center justify-center w-72 h-72 flex-1 my-8 mx-2"
+              >
+                <div className="w-72 mr-4">
+                  <Card
+                    name={name}
+                    votes={votes}
+                    category={category}
+                    picture={picture}
+                    description={description}
+                    lastUpdated={lastUpdated}
+                  />
+                </div>
+              </div>
+            )
+          )}
+        </section>
+      </ScrollContainer>
+    </div>
+  );
+};
+
+export { MobilePreviousRulings };
