81c7076c8590ef1428143d14d340f830cb2146f4
iromero <iromero@lean-tech.io>
2021-11-07 20:39:25 -0500

    feat(previous-rulings): created previous-rulings component

---
+++ components/previous-rulings/index.tsx
@@ -0,0 +1,36 @@
+import ScrollContainer from "react-indiana-drag-scroll";
+import { Card } from "../card";
+import { mockedData } from "../../mocks/data";
+
+const PreviousRulings = (): React.ReactElement => {
+  return (
+    <div>
+      <p className="text-2xl font-light px-3.5 lg:pl-32">Previous Rulings</p>
+      <ScrollContainer className="h-80 flex gap-x-4" vertical={false}>
+        <section className="px-4 mr-2 h-full flex w-full justify-between">
+          {mockedData.data.map(
+            ({ votes, category, name, description, picture, lastUpdated }) => (
+              <div
+                key={name}
+                className="cursor-move flex items-center justify-center w-72 h-72 flex-1 my-8 mx-2"
+              >
+                <div className="w-72 mr-4">
+                  <Card
+                    name={name}
+                    votes={votes}
+                    category={category}
+                    picture={picture}
+                    description={description}
+                    lastUpdated={lastUpdated}
+                  />
+                </div>
+              </div>
+            )
+          )}
+        </section>
+      </ScrollContainer>
+    </div>
+  );
+};
+
+export { PreviousRulings };
